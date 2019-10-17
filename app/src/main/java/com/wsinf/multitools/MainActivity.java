49acd3393cc05d4943f735b16d1dec57badcecb3
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2019-10-17 22:48:22 +0330

    Add first source

---
+++ app/src/main/java/com/wsinf/multitools/MainActivity.java
@@ -0,0 +1,20 @@
+package com.wsinf.multitools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onHelloWold(View view) {
+        Toast.makeText(this, "HEllo", Toast.LENGTH_LONG).show();
+    }
+}
