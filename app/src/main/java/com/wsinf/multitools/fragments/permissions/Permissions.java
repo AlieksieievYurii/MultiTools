1ad211f96f93accba572da70e1cda71259c40ef4
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2019-10-27 18:10:18 +0330

    Create base fragment

---
+++ app/src/main/java/com/wsinf/multitools/fragments/permissions/Permissions.java
@@ -0,0 +1,35 @@
+package com.wsinf.multitools.fragments.permissions;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.wsinf.multitools.R;
+
+public class Permissions extends Fragment {
+
+    private Context context;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_permissions, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
