4541a58a45cf6e0b0e389f7b43e72a94c044f9c0
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2019-11-03 21:31:58 +0330

    Create new actifity for preview taken picture

---
+++ app/src/main/java/com/wsinf/multitools/fragments/camera/ImagePreview.java
@@ -0,0 +1,57 @@
+package com.wsinf.multitools.fragments.camera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.wsinf.multitools.R;
+
+import java.util.Objects;
+
+public class ImagePreview extends AppCompatActivity {
+
+    public static final String IMAGE_EXTRA = "image";
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_preview);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        imageView = findViewById(R.id.img_canvas);
+
+        final Intent intent = getIntent();
+
+        final Bitmap bitmap = intent.getParcelableExtra(IMAGE_EXTRA);
+
+        if (bitmap != null)
+            setImage(bitmap);
+    }
+
+    private void setImage(final Bitmap bitmap) {
+        Matrix matrix = new Matrix();
+
+        matrix.postRotate(90);
+
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight(), true);
+
+        Bitmap rotatedBitmap = Bitmap.createBitmap(scaledBitmap, 0, 0, scaledBitmap.getWidth(), scaledBitmap.getHeight(), matrix, true);
+        imageView.setImageBitmap(rotatedBitmap);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+
+        return super.onOptionsItemSelected(item);
+    }
+}
