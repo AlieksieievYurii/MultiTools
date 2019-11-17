a1d9ba34d77bcec26abb07b5e05ff383536f37e5
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2019-11-17 17:43:12 +0330

    Inializate the Room DataBase in the App class, to avoid creating Singletone

--- app/src/main/java/com/wsinf/multitools/App.java
+++ app/src/main/java/com/wsinf/multitools/App.java
@@ -5,14 +5,25 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.os.Build;
 
+import androidx.room.Room;
+
+import com.wsinf.multitools.fragments.gpstracker.room.LocalDataBase;
+
 public class App extends Application {
     public static final String GPS_TRACKER_CHANNEL_ID = "gps.tracker.channel.id";
+    private static final String LOCAL_DATA_BASE_NAME = "localDB";
+
+    private LocalDataBase localDataBase;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        localDataBase = Room.databaseBuilder(this, LocalDataBase.class, LOCAL_DATA_BASE_NAME).allowMainThreadQueries().build();
         createGpsTrackerNotificationChannel();
+    }
+
+    public LocalDataBase getLocalDataBase() {
+        return localDataBase;
     }
 
     private void createGpsTrackerNotificationChannel() {
