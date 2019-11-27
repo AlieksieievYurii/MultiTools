8462cf5c5cd7d9ed74a96e5d929c85b1a10c5284
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2019-11-27 21:21:52 +0330

    Implement the function to calculate Dew point and absolute humidity

--- app/src/main/java/com/wsinf/multitools/fragments/enviromental/Utils.java
+++ app/src/main/java/com/wsinf/multitools/fragments/enviromental/Utils.java
@@ -2,11 +2,23 @@ package com.wsinf.multitools.fragments.enviromental;
 
 public class Utils {
 
-    static float calculateDewPoint() {
+    private static final double M = 17.62;
+    private static final double TN = 243.12;
+    private static final double A = 6.112;
 
+    static double calculateDewPoint(float temperature, float humidity) {
+        final double a = (M * temperature) / (TN + temperature);
+        final double ln = Math.log(humidity / 100);
+        final double top = ln + a;
+        final double bottom = M - (ln + a);
+
+        return TN * (top/bottom);
     }
 
-    static float calculateAbsoluteHumidity() {
+    static double calculateAbsoluteHumidity(float temperature, float humidity) {
+        final double top = (humidity/100) * A * Math.exp((M*temperature)/(TN + temperature));
+        final double bottom = 273.15 + temperature;
 
+        return 216.7 * (top/bottom);
     }
 }
