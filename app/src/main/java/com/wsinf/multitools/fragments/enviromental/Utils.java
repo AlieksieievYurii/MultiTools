2bb87e6009e08d33677e051fb8e929b2a08fb641
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2019-11-27 21:00:14 +0330

    Create emply class. There will be placed functions for calculating

---
+++ app/src/main/java/com/wsinf/multitools/fragments/enviromental/Utils.java
@@ -0,0 +1,12 @@
+package com.wsinf.multitools.fragments.enviromental;
+
+public class Utils {
+
+    static float calculateDewPoint() {
+
+    }
+
+    static float calculateAbsoluteHumidity() {
+
+    }
+}
