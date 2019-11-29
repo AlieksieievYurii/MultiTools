9990c552a0e84bae9e7484b152420b168d4355aa
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2019-11-29 16:06:12 +0100

    Implement Point object

--- app/src/main/java/com/wsinf/multitools/fragments/spy/point/Point.java
+++ app/src/main/java/com/wsinf/multitools/fragments/spy/point/Point.java
@@ -1,4 +1,75 @@
 package com.wsinf.multitools.fragments.spy.point;
 
-public class Point {
+import com.wsinf.multitools.fragments.spy.Serializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Point implements Serializer {
+    private String id;
+    private float latitude;
+    private float longitude;
+    private long timestamp;
+
+    public Point() {
+    }
+
+    public Point(float latitude, float longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "id='" + id + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+
+    @Override
+    public Map<String, Object> toMap() {
+        final Map<String, Object> map = new HashMap<>();
+        map.put("latitude", latitude);
+        map.put("longitude", longitude);
+        map.put("timestamp", timestamp);
+        return map;
+    }
+
 }
