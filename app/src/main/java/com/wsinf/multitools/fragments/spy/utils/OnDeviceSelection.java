0d3f97ab0f5e646f2be5573744e7471718f15c20
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2019-11-29 18:06:09 +0100

    Create list item

---
+++ app/src/main/java/com/wsinf/multitools/fragments/spy/utils/OnDeviceSelection.java
@@ -0,0 +1,7 @@
+package com.wsinf.multitools.fragments.spy.utils;
+
+import com.wsinf.multitools.fragments.spy.device.Device;
+
+public interface OnDeviceSelection {
+    void onDeviceSelect(final Device device);
+}
