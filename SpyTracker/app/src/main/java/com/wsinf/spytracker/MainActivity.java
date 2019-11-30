bd608225fd0a759c38c73483f163afc482ab012a
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2019-11-30 17:22:28 +0100

    Start creating new application SpyTracker

---
+++ SpyTracker/app/src/main/java/com/wsinf/spytracker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wsinf.spytracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
